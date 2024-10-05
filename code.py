72307dd0adbac16a25561560c4086f1a9661be08
dhafin27gio <113826400+dhafin27gio@users.noreply.github.com>
2024-10-05 11:30:56 +0700

    code.json

--- code.py
+++ code.py
@@ -1,1 +1,15 @@
-
+{
+    "BLUM TRACK QUEST": "Blum - Big City Life",
+    "Pre-Market Trading?": "WOWBLUM",
+    "Doxxing? What's that?": "NODOXXING",
+    "$2.5M+ DOGS Airdrop": "HAPPYDOGS",
+    "Liquidity Pools Guide": "BLUMERSSS",
+    "What Are AMMs?": "CRYPTOSMART",
+    "Say No to Rug Pull!": "SUPERBLUM",
+    "Navigating Crypto": "HEYBLUM",
+    "Secure your Crypto!": "BEST PROJECT EVER",
+    "Forks Explained": "Go Get",
+    "How To Analyze Crypto": "VALUE",
+    "What are Telegram Mini Apps?": "CRYPTOBLUM",
+    "How to Memecoin?": "MEMEBLUM"
+}
